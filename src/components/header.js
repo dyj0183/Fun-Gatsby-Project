422e335474e3b949ade50b212b510ee11642f790
Yu-Chun <dyj0183@byui.edu>
2024-08-28 00:44:49 -0600

    modified header content

--- src/components/header.js
+++ src/components/header.js
@@ -1,27 +1,29 @@
 import * as React from "react"
-import { Link } from "gatsby"
 import styled from "styled-components"
 
 const Header = () => {
   return (
-    <Nav>
-      <NavLink to="/">Yu-Chun Website</NavLink>
-    </Nav>
+    <HeadWrapper>
+      <HeadContent>
+        I'd like to become a
+        <span style={{ fontWeight: "bold", marginLeft: "8px" }}> MVer</span>!
+      </HeadContent>
+    </HeadWrapper>
   )
 }
 
 export default Header
 
-const Nav = styled.nav`
-  background: red;
-  height: 80px;
-  display: flex;
-  justify-content: space-between;
-`
-const NavLink = styled(Link)`
-  color: white;
+const HeadWrapper = styled.div`
+  background: #add8e6;
+  height: 160px;
   display: flex;
   align-items: center;
-  text-decoration: none;
-  cursor: pointer;
+  justify-content: center;
+`
+const HeadContent = styled.div`
+  display: flex;
+  align-items: center;
+  font-size: 50px;
+  text-shadow: 2px 2px 4px rgba(0, 0, 0, 0.5); /* Shadow effect */
 `
